44f659212b894d65c623c38b872346ba391a531f
Anna <anvolkravt@ya.ru>
2025-06-20 20:11:48 +0300

    Lab2

--- 30333_Labs_Kravchenko.UI/Controllers/HomeController.cs
+++ 30333_Labs_Kravchenko.UI/Controllers/HomeController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace _30333_Labs_Kravchenko.UI.Controllers
 {
     public class HomeController : Controller
     {
+        //private readonly ILogger<HomeController> _logger;
+        private readonly List<ListDemo> _listData;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            //_logger = logger;
+            _listData = new List<ListDemo>
+            {
+                new ListDemo {Id=1, Name="Item 1"},
+                new ListDemo {Id=2, Name="Item 2"},
+                new ListDemo {Id=3, Name="Item 3"}
+            };
+        }
+
         public IActionResult Index()
         {
+            ViewData["LabTitle"] = "Лабораторная работа №2";
+            //SelectList data = new SelectList(_listData, "Id", "Name");
+            ViewData["List"] = new SelectList(_listData, "Id", "Name");
             return View();
         }
+        public class ListDemo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
     }
 }
---
+++ 30333_Labs_Kravchenko.UI/Models/Components/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _30333_Labs_Kravchenko.UI.Models.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
