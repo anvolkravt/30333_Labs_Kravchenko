015cef287fc723afba5fdea1143962aa90a8b5db
Anna <anvolkravt@ya.ru>
2025-06-30 14:45:32 +0300

    min stable Razor

--- 30333_Labs_Kravchenko.Blazor/Program.cs
+++ 30333_Labs_Kravchenko.Blazor/Program.cs
@@ -1,32 +1,30 @@
 using _30333_Labs_Kravchenko.Blazor.Components;
 using _30333_Labs_Kravchenko.Blazor.Services;
 using _30333_Labs_Kravchenko.Domain.Entities;
+using Microsoft.AspNetCore.Components.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient<IProductService<Medication>, ApiProductService>(c => c.BaseAddress = new Uri("https://localhost:7002/api/medications"));
-
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHttpClient<IProductService<Medication>, ApiProductService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7002/api/medications/");
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-app.Run();
+await app.RunAsync();
--- 30333_Labs_Kravchenko.Blazor/Services/ApiProductService.cs
+++ 30333_Labs_Kravchenko.Blazor/Services/ApiProductService.cs
@@ -1,49 +1,74 @@
 ﻿using _30333_Labs_Kravchenko.Domain.Entities;
 using _30333_Labs_Kravchenko.Domain.Models;
+using System.Diagnostics;
 
 namespace _30333_Labs_Kravchenko.Blazor.Services
 {
     public class ApiProductService(HttpClient Http) : IProductService<Medication>
     {
-        List<Medication> _medications;
-        int _currentPage = 1;
-        int _totalPages = 1;
+        private List<Medication> _medications = new();
+        private int _currentPage = 1;
+        private int _totalPages = 1;
+
         public IEnumerable<Medication> Products => _medications;
         public int CurrentPage => _currentPage;
         public int TotalPages => _totalPages;
         public event Action ListChanged;
-        public async Task GetProducts(int pageNo, int pageSize)
+
+        public async Task GetProducts(int pageNo = 1, int pageSize = 3)
         {
-            // Url сервиса API
-            var uri = Http.BaseAddress.AbsoluteUri;
-            // данные для Query запроса
-            var queryData = new Dictionary<string, string>
+            try
             {
-                { "pageNo", pageNo.ToString() },
-                {"pageSize", pageSize.ToString() }
-            };
-            var query = QueryString.Create(queryData);
-            // Отправить запрос http
-            var result = await Http.GetAsync(uri + query.Value);
-            // В случае успешного ответа
-            if (result.IsSuccessStatusCode)
-            {
-                // получить данные из ответа
-                var responseData = await result.Content
-                .ReadFromJsonAsync<ResponseData<ProductListModel<Medication>>>();
-                // обновить параметры
-                _currentPage = responseData.Data.CurrentPage;
-                _totalPages = responseData.Data.TotalPages;
-                _medications = responseData.Data.Items;
-                ListChanged?.Invoke();
+                var uri = Http.BaseAddress?.AbsoluteUri.TrimEnd('/') + "/";
+                var queryData = new Dictionary<string, string>
+                {
+                    { "pageNo", pageNo.ToString() },
+                    { "pageSize", pageSize.ToString() }
+                };
+                var query = QueryString.Create(queryData);
+                var fullUri = uri + query.Value.TrimStart('/');
+                Debug.WriteLine($"Requesting: {fullUri}");
+
+                var result = await Http.GetAsync(fullUri);
+                Debug.WriteLine($"Response: {result.StatusCode}");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var responseData = await result.Content.ReadFromJsonAsync<ResponseData<ProductListModel<Medication>>>();
+                    if (responseData?.Success == true && responseData.Data != null)
+                    {
+                        _medications = responseData.Data.Items?.ToList() ?? new List<Medication>();
+                        _currentPage = responseData.Data.CurrentPage;
+                        _totalPages = responseData.Data.TotalPages;
+                        Debug.WriteLine($"Received {_medications.Count} items, page {_currentPage}/{_totalPages}");
+                        foreach (var med in _medications)
+                        {
+                            Debug.WriteLine($"Medication: {med.Name}, ID: {med.Id}");
+                        }
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Response data is null or invalid");
+                        _medications = new List<Medication>();
+                    }
+                }
+                else
+                {
+                    var error = await result.Content.ReadAsStringAsync();
+                    Debug.WriteLine($"Error: {error}");
+                    _medications = new List<Medication>();
+                    _currentPage = 1;
+                    _totalPages = 1;
+                }
             }
-            // В случае ошибки
-            else
+            catch (Exception ex)
             {
-                _medications = null;
+                Debug.WriteLine($"Exception in GetProducts: {ex.Message}");
+                _medications = new List<Medication>();
                 _currentPage = 1;
                 _totalPages = 1;
             }
+            ListChanged?.Invoke();
         }
     }
 }
